b454595e57ea92093acf3f439d9cb02a608d95bf
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-08-23 17:42:53 -0400

    Initial commit of dns spoofer
    This will be the "main" program going forward. Previous script, net_cut.py, was designed to show the packets that were flowing through the spoofing machine

---
+++ dns_spoof.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import netfilterqueue
+
+#iptables -I FORWARD -j NFQUEUE --queue-num 0
+#this is the place where packets are included by default as they flow through the computer
+
+#iptables -I OUTPUT -j NFQUEUE --queue-num 0
+#this will capture all the outgoing packets of the current machine
+
+#iptables -I INPUT -j NFQUEUE --queue-num 0
+#this will capture all the incoming packets of the current machine
+
+#iptables --flush
+#clear out IP tables
+
+def process_packet(packet):
+    print(packet)
+    packet.accept()
+    #packet.drop()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
