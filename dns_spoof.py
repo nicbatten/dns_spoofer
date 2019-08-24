32870d7d773ba0dd4c5e9554ee39f45f929ded59
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-08-24 15:05:05 -0400

    Updated some typos
    Fixed a glaringly obvious typo that was causing the packet to not display on the screen correctly

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import netfilterqueue
+import scapy.all as scapy
 
 #iptables -I FORWARD -j NFQUEUE --queue-num 0
 #this is the place where packets are included by default as they flow through the computer
@@ -14,9 +15,10 @@ import netfilterqueue
 #clear out IP tables
 
 def process_packet(packet):
-    print(packet)
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        print(scapy_packet.show())
     packet.accept()
-    #packet.drop()
 
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
